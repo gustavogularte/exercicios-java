d08b6c07fc64f0b59623651f02b5e5c587644592
gustavorrarend@gmail.com <gustavorrarend@gmail.com>
2026-06-14 19:05:31 -0300

    geração caldeira curso java

---
+++ GC-Curso-Java/src/DesafioTemperatura.java
@@ -0,0 +1,9 @@
+public class DesafioTemperatura {
+    static void main() {
+        double temperaturaCelcius = 30.4;
+        double temperaturaFahrenheit = (temperaturaCelcius * 1.8) + 32;
+        System.out.println("Temperatura em fahrenheit: " + temperaturaFahrenheit);
+        int temperaturaFahrenheitInt = (int) temperaturaFahrenheit;
+        System.out.println("Temperatura Fahrenheit: " + temperaturaFahrenheitInt);
+    }
+}
